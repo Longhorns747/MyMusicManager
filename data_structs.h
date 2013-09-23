66836a2597e73b9dad7c9f643c5c28c28456a588
Longhorns747 <eshernan3@gatech.edu>
2013-09-23 18:50:55 -0400

    Added data structures

---
+++ data_structs.h
@@ -0,0 +1,35 @@
+#ifndef DATA_STRUCTS_H_INCLUDED
+#define DATA_STRUCTS_H_INCLUDED
+
+#include <stdio.h>		    /* for printf() and fprintf() */
+#include <sys/socket.h>		/* for socket(), connect(), send(), and recv() */
+#include <arpa/inet.h>		/* for sockaddr_in and inet_addr() */
+#include <stdlib.h>
+
+typedef enum {
+	LIST, PULL, DIFF, LEAVE
+} message_type;
+
+typedef struct 
+{
+	int ID;
+	char[] filename;
+	size_t num_bytes;
+	unsigned char payload[num_bytes];
+} Music_File;
+
+typedef struct
+{
+	int[] music_ids;
+	//Not sure how to represent host here... int for IP? Char[]?
+} Filestate;
+
+typedef struct
+{
+	message_type type;
+	Filestate state;
+} Message;
+
+
+
+#endif
